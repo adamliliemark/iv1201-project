731dd799acc3b92d6d9bfa467b657899a5cb4730
Adam CL <adamliliemark@gmail.com>
2021-01-19 15:34:47 +0100

    tests

--- src/test/java/com/iv1201/project/recruitment/RecruitmentApplicationTests.java
+++ src/test/java/com/iv1201/project/recruitment/RecruitmentApplicationTests.java
@@ -1,13 +1,18 @@
 package com.iv1201.project.recruitment;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class RecruitmentApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private AppController controller;
 
+    @Test
+    void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
 }
