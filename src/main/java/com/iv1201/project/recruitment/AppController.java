25dec45963e88a525b0175ff0ca985380ab0c371
Adam CL <adamliliemark@gmail.com>
2021-01-19 10:08:09 +0100

    second initial

---
+++ src/main/java/com/iv1201/project/recruitment/AppController.java
@@ -0,0 +1,26 @@
+package com.iv1201.project.recruitment;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AppController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("msg", "hejsan");
+        model.addAttribute("loggedIn", false);
+        return "home";
+    }
+
+    @PostMapping("/")
+    public String post(Model model) {
+        model.addAttribute("msg", "svejsan");
+        model.addAttribute("loggedIn", true);
+        return "home";
+    }
+
+}
