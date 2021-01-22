af33fb194539f621190e0a3a870e6a28a8965e6c
Magnus <krummelur@gmail.com>
2021-01-22 11:13:10 +0100

    added default user and admin

--- src/main/java/com/iv1201/project/recruitment/persistence/AuthorityRepo.java
+++ src/main/java/com/iv1201/project/recruitment/persistence/AuthorityRepo.java
@@ -3,9 +3,7 @@ package com.iv1201.project.recruitment.persistence;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface UserRepository extends CrudRepository<User, String> {
-    Optional<User> findByEmail(String email);
+public interface AuthorityRepo extends CrudRepository<Authority, String> {
+
 }
