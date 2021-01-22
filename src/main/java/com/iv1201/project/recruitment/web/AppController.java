36dbfd456792d8896344551724ac19687a5ebc42
Adam CL <adamliliemark@gmail.com>
2021-01-22 11:48:32 +0100

    basic non-mwe

--- src/main/java/com/iv1201/project/recruitment/web/AppController.java
+++ src/main/java/com/iv1201/project/recruitment/web/AppController.java
@@ -8,9 +8,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.annotation.PostConstruct;
+import java.util.HashMap;
 import java.util.Optional;
 
 @Controller
@@ -81,8 +83,25 @@ public class AppController {
         return "create_account";
     }
 
+    HashMap<String, Integer> exp = new HashMap<>();
     @GetMapping("/apply")
-    public String applyForPosition(Model model) {
+    public String applyForPosition(@ModelAttribute Appl appl, Model model) {
+        appl.setExpertise("Hot dogs");
+        appl.setYears(39);
+        model.addAttribute("application", appl);
+        System.out.println(appl.getExpertise());
+        System.out.println(appl.getYears());
+        return "apply_for_position";
+    }
+
+    @PostMapping("/apply")
+    public String fetchApplication(@ModelAttribute Appl appl, Model model) {
+        String exps = appl.getExpertise();
+        int years = appl.getYears();
+        System.out.println(exps);
+        exp.put(exps, years);
+        model.addAttribute("experiences", exp);
+        model.addAttribute("application", appl);
         return "apply_for_position";
     }
 }
