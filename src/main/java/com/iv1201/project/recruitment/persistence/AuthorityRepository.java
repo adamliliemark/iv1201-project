03ba9504f143cc4a37bc70fa7a5822ae01af23a6
Magnus <krummelur@gmail.com>
2021-01-22 12:29:12 +0100

    added a service

--- src/main/java/com/iv1201/project/recruitment/persistence/AuthorityRepository.java
+++ src/main/java/com/iv1201/project/recruitment/persistence/AuthorityRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuthorityRepo extends CrudRepository<Authority, String> {
+public interface AuthorityRepository extends CrudRepository<Authority, String> {
 
 }
--- src/main/java/com/iv1201/project/recruitment/service/UserService.java
+++ src/main/java/com/iv1201/project/recruitment/service/UserService.java
@@ -1,4 +1,4 @@
-package com.iv1201.project.recruitment;
+package com.iv1201.project.recruitment.service;
 
 import com.iv1201.project.recruitment.persistence.Authority;
 import com.iv1201.project.recruitment.persistence.AuthorityRepository;
@@ -11,40 +11,55 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 
+
 @Component
 @Service
-public class UserService implements IUserService {
+public class UserService {
+    public enum Role {
+        ROLE_USER,
+        ROLE_ADMIN
+    }
 
     @Autowired
     UserRepository userRepo;
     @Autowired
     AuthorityRepository authorityRepo;
 
-
-    @PostConstruct
-    public void addDefaultUsers() {
-        System.err.println("Saving users!");
-        if(!userRepo.findByEmail("testuser@example.com").isPresent()) {
-            User user = new User(
-                    "testuser@example.com",
-                    "userFirstName",
-                    "userLastName",
-                    1000L,
-                    new BCryptPasswordEncoder().encode("pass"));
-            Authority userAuth = new Authority("ROLE_USER", user);
+    /**
+     * Adds a new user into the system, if it is valid.
+     * This is example functionality that a service provides.
+     * Validating business logic and interacting with the data store.
+     */
+    public void addNewUser(String email, String firstName, String lastName, String clearTextPassword, Role role, Long ssn) {
+       boolean isValid = true;
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        if(isValid) {
+            User user = new User(email, firstName, lastName, ssn, encoder.encode(clearTextPassword));
+            Authority userAuth = new Authority(role.toString(), user);
             userRepo.save(user);
             authorityRepo.save(userAuth);
         }
+    }
+
+    @PostConstruct
+    public void addDefaultUsers() {
+        if(!userRepo.findByEmail("testuser@example.com").isPresent()) {
+            System.err.println("Saving test user!");
+            addNewUser("testuser@example.com",
+                    "userFirstName",
+                    "userLastName",
+                    "pass",
+                    Role.ROLE_USER,
+                    1000L);
+        }
         if(!userRepo.findByEmail("testadmin@example.com").isPresent()) {
-            User admin = new User(
-                    "testadmin@example.com",
+            System.err.println("Saving test admin!");
+            addNewUser("testadmin@example.com",
                     "adminFirstName",
                     "adminLastName",
-                    1000L,
-                    new BCryptPasswordEncoder().encode("pass"));
-            Authority adminAuth = new Authority("ROLE_ADMIN", admin);
-            userRepo.save(admin);
-            authorityRepo.save(adminAuth);
+                    "pass",
+                    Role.ROLE_ADMIN,
+                    1000L);
         }
     }
 }
