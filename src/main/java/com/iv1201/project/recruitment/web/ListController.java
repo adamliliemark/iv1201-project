56beb1dd290580be07280a7875f8f5fa04a43fec
Fredrik Öberg <fredrik.oberg@gmx.com>
2021-01-29 18:44:58 +0100

    forms working

--- src/main/java/com/iv1201/project/recruitment/web/ListController.java
+++ src/main/java/com/iv1201/project/recruitment/web/ListController.java
@@ -1,20 +1,17 @@
 package com.iv1201.project.recruitment.web;
 
-import com.iv1201.project.recruitment.model.AvailableParameters;
 import com.iv1201.project.recruitment.persistence.*;
 import com.iv1201.project.recruitment.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
-import java.lang.reflect.Parameter;
-import java.security.Principal;
-
 
 
 @Controller
@@ -27,7 +24,7 @@ public class ListController {
     private CompetenceRepository competenceRepo;
 
     @GetMapping("/list")
-    public String list(Model model){
+    public String list(@Valid @ModelAttribute("listFormObject") ListForm listForm, Model model){
 
 //        Iterable<Competence> all = competenceRepo.findAll();
 //
@@ -35,21 +32,19 @@ public class ListController {
 //
 //        all.forEach(c -> stringBuilder.append(c.getId() + " ").append(c.getName() + "\n")
 //        );
-//        model.addAttribute("competence", stringBuilder.toString());
 
         model.addAttribute("listFormObject", new ListForm());
-        model.addAttribute("months", new ListForm().getMonths());
-        model.addAttribute("years", new ListForm().getYears());
+        model.addAttribute("expertise", competenceRepo.findAll());
+
 
         return "list";
     }
 
     @PostMapping("/list/applications")
-    public String listParameters(@Valid @ModelAttribute ListForm listForm ,Principal principal, Model model){
+    public String listParameters(@Valid @ModelAttribute("listFormObject") ListForm listForm , BindingResult bindingResult, Model model){
 
         model.addAttribute("listFormObject", new ListForm());
-        model.addAttribute("months", new ListForm().getMonths());
-        model.addAttribute("years", new ListForm().getYears());
+        model.addAttribute("expertise", competenceRepo.findAll());
         return "list";
     }
 
--- src/main/java/com/iv1201/project/recruitment/web/ListForm.java
+++ src/main/java/com/iv1201/project/recruitment/web/ListForm.java
@@ -1,55 +1,24 @@
 package com.iv1201.project.recruitment.web;
 
-
-import java.text.DateFormatSymbols;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-
+import java.time.LocalDate;
 
 public class ListForm {
 
-    private ArrayList<String> months;
-    private ArrayList<String> years;
-    private String date;
+
+    private LocalDate date;
     private String competence;
     private String name;
+    private AvailabilityForm availabilityForm;
 
     ListForm(){
-        setMonths();
-        setYears();
+        this.availabilityForm = new AvailabilityForm();
     }
 
-    private void setMonths(){
-        months = new ArrayList<>();
-        months.addAll(Arrays.asList(new DateFormatSymbols().getMonths()));
-    }
-
-    public ArrayList<String> getMonths() {
-        return months;
-    }
-
-    public ArrayList<String> getYears() {
-        return years;
-    }
-
-    private void setYears() {
-
-        int lowYear = 1980;
-        int highYear = Calendar.getInstance().get(Calendar.YEAR);
-        years = new ArrayList<>();
-
-        for (int i = lowYear; i <= highYear; i++)
-          years.add(Integer.toString(i));
-
-        years.add("");
-    }
-
-    public String getDate() {
+    public LocalDate getDate() {
         return date;
     }
 
-    public void setDate(String date) {
+    public void setDate(LocalDate date) {
         this.date = date;
     }
 
@@ -67,5 +36,13 @@ public class ListForm {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public AvailabilityForm getAvailabilityForm() {
+        return availabilityForm;
+    }
+
+    public void setAvailabilityForm(AvailabilityForm availabilityForm) {
+        this.availabilityForm = availabilityForm;
     }
 }
