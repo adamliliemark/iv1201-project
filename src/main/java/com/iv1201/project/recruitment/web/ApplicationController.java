76e711f80a6aa994e5a2e9facb0335a624c2147e
adamcl <43515905+adamliliemark@users.noreply.github.com>
2021-02-05 10:45:03 +0100

    overhaul of views and application controller (#42)
    * overhaul of views and application controller
    
    * login redesign

--- src/main/java/com/iv1201/project/recruitment/web/ApplicationController.java
+++ src/main/java/com/iv1201/project/recruitment/web/ApplicationController.java
@@ -4,7 +4,6 @@ import com.iv1201.project.recruitment.persistence.*;
 import com.iv1201.project.recruitment.service.CompetenceService;
 import com.iv1201.project.recruitment.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Scope;
 import com.iv1201.project.recruitment.persistence.Availability;
 import com.iv1201.project.recruitment.persistence.User;
@@ -31,10 +30,7 @@ import java.util.*;
 public class ApplicationController {
 
     @Autowired
-    MessageSource messageSource;
-
-    @Autowired
-    UserService userService;
+    private UserService userService;
 
     @Autowired
     private CompetenceService competenceService;
@@ -42,15 +38,21 @@ public class ApplicationController {
     private User user;
     private Map<String, Competence> competences;
 
+    /**
+     * startApplication is the starting point of an application.
+     * @param locale the user's locale which determines the translation table in the database
+     * @param competenceFormObject the object that fetches the competenceForm
+     * @param principal the logged in user
+     * @param model for thymeleaf
+     * @return applicationView with competence form
+     */
     @GetMapping("/apply")
-    public String startApplication(Locale locale, @ModelAttribute("competenceFormObject") CompetenceForm competenceForm, Principal principal, Model model) {
-        System.out.println(locale);
-
+    public String startApplication(Locale locale, @ModelAttribute("competenceFormObject") CompetenceForm competenceFormObject, Principal principal, Model model) {
         Optional<User> userMaybe = userService.findByEmail(principal.getName());
-        if(!userMaybe.isPresent())
+        if(!userMaybe.isPresent()) {
             throw new RuntimeException("Expected a user to exist on protected endpoint");
-        competences = competenceService.getAllWithLocalNames("en_US");
-
+        }
+        competences = competenceService.getAllWithLocalNames(locale.toString());
         user = userMaybe.get();
         model.addAttribute("form", "competence");
         model.addAttribute("user", user);
@@ -58,17 +60,24 @@ public class ApplicationController {
         return "applicationView";
     }
 
+    /**
+     * Mapping to fetch the competence form.
+     * @param competenceFormObject the filled out form from thymeleaf
+     * @param bindingResult validation of the form
+     * @param model for thymeleaf
+     * @return applicationView with competence form
+     */
     @PostMapping("/apply/expertise")
-    public String fetchCompetenceForm(@Valid @ModelAttribute("competenceFormObject") CompetenceForm competenceForm, BindingResult bindingResult, Model model) {
+    public String fetchCompetenceForm(@Valid @ModelAttribute("competenceFormObject") CompetenceForm competenceFormObject, BindingResult bindingResult, Model model) {
         if(!bindingResult.hasErrors()) {
-            if(!competences.containsKey(competenceForm.getName()))
+            if(!competences.containsKey(competenceFormObject.getName())) {
                 throw new RuntimeException("Expected the supplied competence to exist in the database.");
-            user.addCompetence(competences.get(competenceForm.getName()), competenceForm.getYears());
+            }
+            user.addCompetence(competences.get(competenceFormObject.getName()), competenceFormObject.getYears());
         }
         if(checkForLastFetch(user)) {
             model.addAttribute("last", true);
         }
-
         model.addAttribute("user", user);
         model.addAttribute("form", "competence");
         model.addAttribute("availableExpertises", competences.keySet());
@@ -79,10 +88,17 @@ public class ApplicationController {
         return user.getCompetences().size() == competences.size();
     }
 
+    /**
+     * Mapping to fetch the availability form.
+     * @param availabilityFormObject the filled out form from thymeleaf
+     * @param bindingResult validation of the form
+     * @param model for thymeleaf
+     * @return applicationView with availability form
+     */
     @PostMapping("/apply/availability")
-    public String fetchAvailabilityForm(@Valid @ModelAttribute("availabilityFormObject") AvailabilityForm availabilityForm, BindingResult bindingResult, Model model) {
+    public String fetchAvailabilityForm(@Valid @ModelAttribute("availabilityFormObject") AvailabilityForm availabilityFormObject, BindingResult bindingResult, Model model) {
         if(!bindingResult.hasErrors()) {
-            user.setAvailability(new Availability(availabilityForm.getFrom(), availabilityForm.getTo()));
+            user.setAvailability(new Availability(availabilityFormObject.getFrom(), availabilityFormObject.getTo()));
         }
         model.addAttribute("user", user);
         model.addAttribute("form", "availability");
@@ -90,8 +106,9 @@ public class ApplicationController {
     }
 
     /**
-     * Final page of an application. Reviews the user's application.
+     * Delivers the review page.
      * @param model for Thymeleaf
+     * @return applicationView with review form
      */
     @PostMapping("/apply/review")
     public String reviewApplication(Model model) {
@@ -100,11 +117,16 @@ public class ApplicationController {
         return "applicationView";
     }
 
+    /**
+     * Saves the application and redirects the user back to their start page.
+     * @param model for thymeleaf
+     * @return back to start page
+     */
     @PostMapping("/apply/submit")
     public String submittedApplication(Model model) {
         user = userService.saveUser(user);
         model.addAttribute("user", user);
         model.addAttribute("form", "submitted");
-        return "applicationView";
+        return "redirect:/";
     }
 }
