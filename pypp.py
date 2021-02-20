a747ac881047a9924098af4db24c5b7504cb2e2d
adamcl <adamliliemark@gmail.com>
2021-02-20 14:56:43 +0100

    new test

--- pypp.py
+++ pypp.py
@@ -1,9 +1,11 @@
 
 import asyncio
+import time
 from pyppeteer import launch
 
 
 async def main():
+    sleep(15)
     browser = await launch(options={'args': ['--no-sandbox']})
     page = await browser.newPage()
     await page.goto('http://localhost:8080/apply')
