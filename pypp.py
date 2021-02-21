d818d8c76b5459a78f9daa9dd4d71973821ca32a
adamcl <adamliliemark@gmail.com>
2021-02-21 22:00:06 +0100

    some refactoring and inconsistencies in how stuff works...

--- pypp.py
+++ pypp.py
@@ -1,14 +1,15 @@
 
 import asyncio
-import time
 from pyppeteer import launch
 import pyppeteer as pypp
 
 BASE_URL = "http://127.0.0.1:8080"
-WAIT_OPTS =  {"waitUntil":"networkidle0"}
+WAIT_OPTS = {"waitUntil": "networkidle0"}
+
 
 def printTestCaseDesc(desc):
     print(" - {}".format(desc))
+
 
 async def retryConnect(url, retries, page):
     if retries <= 0:
@@ -23,10 +24,10 @@ async def retryConnect(url, retries, page):
             await asyncio.sleep(4)
             await retryConnect(url, retries-1, page) 
 
+
 async def main():
     browser = await launch(
-        options=
-        {
+        options={
             'args': ['--no-sandbox --lang=en_US']
         })
     page = await browser.newPage()
@@ -34,7 +35,7 @@ async def main():
 
     await login(page)
     await check_first_page(page)
-    await page.click("#apply-link",WAIT_OPTS)
+    await page.click("#apply-link", WAIT_OPTS)
     await check_translation_table(page)
     await enter_and_check_competence_years(page)
     # raise Exception("this is a dummy exception that should kill the process")
@@ -58,9 +59,6 @@ async def check_first_page(page):
     assert (await page.JJeval(".home-middle", "node => node.map(n => n.innerText)")) == ["You are a simple user..."]
 
 
-# this obviously needs to switch to Swedish and be tried again.
-
-
 async def check_translation_table(page):
     await page.waitForSelector("#competence")
     printTestCaseDesc("Checking that the value in the competences list is translated to English")
@@ -70,12 +68,19 @@ async def check_translation_table(page):
     for competence in expectedCompetences:
         assert competence in competences, "Expected competence {} not in competence selector".format(competence)
 
+
 async def enter_and_check_competence_years(page):
     printTestCaseDesc("Entering 2.0 to competence years and checks that it is updated on the page")
     await page.content()
     years = await page.J("#competenceYears")
+    competence_to_choose = "Grilling sausage"
+    await page.select("#competence", competence_to_choose)
     await years.type("2.0")
-    # await page.screenshot({'path': 'competence_years.png'})
+    await page.click("#competenceFormSubmit", WAIT_OPTS)
+    user_competences = await page.JJeval("#userCompetences", "node => node.map(n => n.innerText)")
+    print(user_competences)
+    await page.screenshot({'path': 'competence_years.png', 'fullPage': True})
+
 
 
 asyncio.get_event_loop().run_until_complete(main())
