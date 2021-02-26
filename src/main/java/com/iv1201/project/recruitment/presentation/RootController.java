79b22e2e68981926bbb4c3d304bcafa1044940d3
Adam CL <adamliliemark@gmail.com>
2021-02-26 13:49:24 +0100

    added 404 functionality

--- src/main/java/com/iv1201/project/recruitment/presentation/RootController.java
+++ src/main/java/com/iv1201/project/recruitment/presentation/RootController.java
@@ -1,9 +1,13 @@
 package com.iv1201.project.recruitment.presentation;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
 import javax.servlet.http.HttpServletRequest;
+
 
 /**
  * RootController controls the user's rootview, i.e. delivers either
@@ -40,5 +44,11 @@ public class RootController {
     @GetMapping("/home")
     public String home() {
         return "homeView";
+    }
+
+
+    @GetMapping("/*")
+    public void notFound(HttpHeaders headers, HttpServletRequest request) throws NoHandlerFoundException {
+        throw new NoHandlerFoundException(request.getMethod(), request.getRequestURL().toString(), headers);
     }
 }
