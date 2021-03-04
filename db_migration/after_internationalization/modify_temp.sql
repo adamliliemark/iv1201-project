560290f3d19b306b4fecd78dd8ad5c3fee7793d4
Magnus <krummelur@gmail.com>
2021-03-04 20:44:58 +0100

    added forgotten script changes

--- db_migration/after_internationalization/modify_temp.sql
+++ db_migration/after_internationalization/modify_temp.sql
@@ -35,21 +35,24 @@ create table authorities (
 );
 
 insert into users
-select name as first_name, surname as last_name, ssn, email, password from person where email is not null;
+select name as first_name, surname as last_name, ssn, email, password from person 
+where email is not null
+and password is not null;
 
 insert into unmigrated_person
-select * from person where email is null;
+select * from person where email is null or password is null;
 
 insert into authorities (user_email, authority)
-select email, "ROLE_USER" from person where role_id = 1 and email is not null;
+select email, "ROLE_USER" from person where role_id = 1 and email is not null and password is not null;
 
 insert into authorities (user_email, authority) 
-select email as user_email, "ROLE_ADMIN" from person where role_id = 2 and email is not null;
+select email as user_email, "ROLE_ADMIN" from person where role_id = 2 and email is not null and password is not null;
 
 insert into availability_ (user_email, from_date, to_date)
 select email, from_date, to_date from person, availability
 where availability.person_id = person.person_id
-and email is not null;
+and email is not null
+and password is not null;
 
 
 CREATE TABLE unmigrated_availability (
@@ -62,7 +65,7 @@ to_date DATE
 CREATE TABLE unmigrated_competence_profile (
 competence_profile_id BIGINT PRIMARY KEY,
 person_id BIGINT,
-competence_id BIGINT,
+competence_name varchar(255),
 years_of_experience NUMERIC(4,2)
 );
 
@@ -90,9 +93,10 @@ insert into unmigrated_availability(availability_id, person_id, from_date, to_da
 select availability_id, availability.person_id, from_date, to_date from availability, unmigrated_person
 where unmigrated_person.person_id = availability.person_id;
 
-insert into unmigrated_competence_profile(competence_profile_id, person_id, competence_id, years_of_experience)
-select competence_profile_id, competence_profile.person_id, competence_id, years_of_experience from competence_profile, unmigrated_person
-where unmigrated_person.person_id = competence_profile.person_id;
+insert into unmigrated_competence_profile(competence_profile_id, person_id, competence_name, years_of_experience)
+select competence_profile_id, competence_profile.person_id, competence.name, years_of_experience from competence_profile, competence,unmigrated_person
+where unmigrated_person.person_id = competence_profile.person_id
+and competence.competence_id = competence_profile.competence_id;
 
 
 insert into competence_profile_ (user_email, competence_id, years_of_experience)
@@ -108,4 +112,3 @@ alter table competence drop column name;
 rename table competence_profile_ TO competence_profile;
 rename table availability_ TO availability;
 alter table competence change competence_id id BIGINT;
-drop table competence;
